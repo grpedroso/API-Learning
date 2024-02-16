635c14d6825e4df9e532dd7b9921f5a0aba64773
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2024-02-16 18:29:09 -0300

    Introduzindo API externa

---
+++ ScreenSound-04/Program.cs
@@ -0,0 +1,16 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
+        Console.Write("Insira o numero A: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Insira o numero B: ");
+        int b = int.Parse(Console.ReadLine());
+        int c = a / b;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Problema indentificado: {ex.Message}");
+    }
+}
